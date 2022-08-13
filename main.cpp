6972c6b51bf9e82561e54cd1d6d165f64f02fa5f
Leo <83018357+TheRealNeZuX@users.noreply.github.com>
2022-08-13 13:10:21 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <stdexcept>
+
+template<class T, int length>
+class SmartPointer{
+public:
+    const int& size = size_;
+    SmartPointer(int val){
+        if(length>1){
+            obj = new T[length];
+            *obj = val;
+        }else if(length==1){
+            obj = new T;
+        }
+        size_ = length;
+    }
+    SmartPointer(){
+        if(length>1){
+            obj = new T[length];
+        }else if(length==1){
+            obj = new T;
+        }
+        size_ = length;
+    }
+    ~SmartPointer(){
+        if(length>1){
+            delete[] obj;
+        }else if(length==1){
+            delete obj;
+        }
+    }
+    T& operator*(){
+        return *obj;
+    }
+    T& operator[](int index){
+        if (index<length){
+            return obj[index];
+        }
+        throw std::out_of_range("You are accessing forbidden memory");
+    }
+    T& operator+(int index){
+        if (index<length){
+            return obj[index];
+        }
+        throw std::out_of_range("You are accessing forbidden memory");
+    }
+    T* operator~(){
+        return obj;
+    }
+
+private:
+    T* obj;
+    int size_;
+};
+
+int main(){
+    SmartPointer<int, 10> a;
+    std::cout<<~a;
+}
